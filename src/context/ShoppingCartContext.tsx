5825e18957a9bf77e8243d97d99cfa78717bfb1c
Uche Chris <chrisuche58@yahoo.com>
2023-04-25 21:03:08 +0100

    update

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useContext } from "react";
+import { ReactNode, createContext, useContext, useState } from "react";
 
 type ShoppingCartContext = {
     getItemQuantity: (id: number) => number
@@ -21,8 +21,38 @@ type ShoppingCartContextProps = {
     children: ReactNode
 }
 
+type CartItem ={
+    id: number
+    quantity: number
+}
+
 export function ShoppingCartProvider({ children }: ShoppingCartContextProps) {
-    return <ShoppingCartContext.Provider value={{}}>
+    const [cartItems, setCartItems] = useState<CartItem[]> ([])
+
+    function getItemQuantity(id:number) {
+        return cartItems.find(item => item.id === id)?.quantity || 0
+        
+    }
+
+    function increaseCartQuantity(id:number) {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id === id) == null) {
+                return [...currItems, {id, quantity: 1}]
+            } else {
+                return currItems.map(item => {
+                    if (item.id === id) {
+                        return { ..item,quantity: item.quantity + 1}
+                    } else { return item 
+                    }
+                }) 
+            }
+
+        })
+        
+    }
+
+
+    return <ShoppingCartContext.Provider value={{getItemQuantity}}>
         {children}
     </ShoppingCartContext.Provider>
 }
