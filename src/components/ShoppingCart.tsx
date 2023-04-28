41dd128e03ff337ddf41214012334404391a4d03
Uche Chris <chrisuche58@yahoo.com>
2023-04-28 15:46:54 +0100

    update

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -8,15 +8,20 @@ type ShoppingCartProps = {
     isOpen: boolean
 }
 
-export function ShoppingCart({ isOpen }) {
-    const { closeCart} = useShoppingCart()
+export function ShoppingCart({ isOpen } : ShoppingCartProps) {
+    const { closeCart, cartItems } = useShoppingCart()
     return (
             <Drawer open={isOpen} onClose={closeCart} direction='right'>
                 <span className='flex items-center p-1 justify-between '>
                 <div>Hello World</div>
                 <XMarkIcon color="black" className="h-5 w-5 cursor-pointer" />
-
                 </span>
+                <body>
+                    <div className='flex flex-col gap-3'>
+                        {cartItems.map(item => 
+                        <CartItem key={cartItems.id} {...item} />)}
+                    </div>
+                </body>
             </Drawer>
     )
 }
