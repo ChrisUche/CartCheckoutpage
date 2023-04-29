a3a944d4f17da5818a2a612cde80e9d24ae1de69
Uche Chris <chrisuche58@yahoo.com>
2023-04-29 14:10:32 +0100

    update

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -12,16 +12,16 @@ export function CartItem({ id, quantity }: CartItemProps) {
     const item = storeItems.find(i => i.id === id)
     if (item == null ) return null
     return (
-        <div className="flex flex-row gap-2 items-center">
-            <img src={item.imgUrl}  className=" h-20 w-32 rounded-sm object-cover"/>
+        <div className="flex flex-row gap-2 items-center p-2">
+            <img src={item.imgUrl}  className=" h-20 w-20 rounded-sm object-cover"/>
             <div className="me-auto">
                 <div>
                     {item.name} {quantity > 1 && <span className=" text-xs opacity-70">x{quantity}</span>}
                 </div>
-            <div className=" text-xs font-mono">{formatCurrency(item.price)}</div>
+            <div className=" text-xs/[17px]  font-mono">{formatCurrency(item.price)}</div>
             </div>
             <div className=" text-xs font-mono">{formatCurrency(item.price * quantity)}</div>
-            <button className="w-5 h-5 rounded bg-slate-500" onClick={() => removeFromCart(item.id)}>&times;</button>
+            <button className="w-5 h-5 rounded" onClick={() => removeFromCart(item.id)}>&times;</button>
 
         </div>
     )
